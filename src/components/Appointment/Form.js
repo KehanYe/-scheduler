54028d8543f840e1c3757c9aa235816306788108
KehanYe <kehanfu@gmail.com>
2022-04-21 00:03:14 -0600

    reset input with reset() and cancel()

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -10,6 +10,15 @@ export default function Form (props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = () => {
+    setStudent('');
+    setInterviewer('null');
+  }
+
+  const cancel = () => {
+    reset();
+    onCancel();
+  }
 
 	return (
     <main className="appointment__card appointment__card--create">
@@ -33,8 +42,8 @@ export default function Form (props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
-          <Button confirm oncliek={onSave(student, interviewer)}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
