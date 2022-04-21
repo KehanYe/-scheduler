45d19338ea27efa161d21cf7fd0a3cdc79dfdb20
KehanYe <kehanfu@gmail.com>
2022-04-21 01:21:26 -0600

    add story: appointment booked and empty

--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
 
 import "index.scss";
 
@@ -212,4 +211,20 @@ storiesOf("Appointments", module)
       onSave={action("onSave")} 
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ))
