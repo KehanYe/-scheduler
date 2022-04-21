1a17bf9ff84dd6d3a0c00a46c218e107f0336a98
KehanYe <kehanfu@gmail.com>
2022-04-21 08:43:16 -0600

    code refactor

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -7,10 +7,6 @@ import 'components/InterviewerList.scss';
 export default function InterviewerList(props) {
   const {interviewers, value, onChange} = props; // value is an interview:number representing currently selected interviewer
 
-  // console.log("props passed into InterviewerList", props)
-  // console.log("interviewers passed into InterviewerList", interviewers)
-  // console.log("value passed into InterviewerList", value)
-  console.log("setInterviewer passed into InterviewerList", onChange)
 
 
 	const interviewerList = interviewers.map(interviewer => (
