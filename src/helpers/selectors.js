2c458f38fada1a4c90c7b09f4a99ad8bb7342910
KehanYe <kehanfu@gmail.com>
2022-04-22 16:00:45 -0600

    getAppointmentsForDay selector and Test

---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export function getAppointmentsForDay(state, day) {
+  if (state.days.length === 0) return []
+  const foundDay = state.days.find(days => days.name === day);
+  
+  // console.log("this is found day", foundDay)
+  if (!foundDay) return [];
+
+  return foundDay.appointments.map(element => state.appointments[element])
+  //Steps to developing selector, to be REFACTORED
+  // const appointments = foundDay.appointments;
+  // const appointmentDetails = appointments.map(element => state.appointments[element]);
+
+  // return foundDay (for testing console.log)
+  // return appointments
+  // return appointmentDetails;
+};
