c9eec50a014f415fb3190e907beb2125645c4d1d
KehanYe <kehanfu@gmail.com>
2022-04-23 16:32:54 -0600

    slight refactor

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,16 +1,18 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   if (!state.days) return []
-  const foundDay = state.days.find(days => days.name === day);
+  const foundDay = state.days.find(selectedDay => selectedDay.name === day);
   
-  // console.log("this is found day", foundDay)
+  console.log("this is selectedDay from foundDay", foundDay)
+  // => foundDay is an object with properity that is array of appointments
   if (!foundDay) return [];
 
   return foundDay.appointments.map(element => state.appointments[element])
+
   //Steps to developing selector, to be REFACTORED
-  // const appointments = foundDay.appointments;
+  // const appointments = foundDay.appointments; => this as array
   // const appointmentDetails = appointments.map(element => state.appointments[element]);
 
-  // return foundDay (for testing console.log)
-  // return appointments
-  // return appointmentDetails;
+  // 1) return foundDay (for testing console.log)
+  // 2) return appointments
+  // 3) return appointmentDetails;
 };
