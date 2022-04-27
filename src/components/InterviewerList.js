2db4db9412eb6fd4453e7273ff92d8e7a3b1eb23
KehanYe <kehanfu@gmail.com>
2022-04-26 23:24:42 -0600

    initalize prop-types with InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import InterviewerListItem from 'components/InterviewerListItem';
 
 // Sass Styling
@@ -25,4 +26,9 @@ export default function InterviewerList(props) {
 			<ul className="interviewers__list">{interviewerList}</ul>
 		</section>
 	);
+
+}
+
+InterviewerList.propTypes = {
+  interviewerList: PropTypes.array.isRequired
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,7 +3,7 @@ import { React, useState } from 'react';
 export default function useVisualMode(initial) {
 	// const [ mode, setMode ] = useState(initial);
 	const [ history, setHistory ] = useState([ initial ]);
-	console.log("useVisualMode history", history)
+	// console.log("useVisualMode history", history)
 
 	//takes a new mode and sets mode, and updates history
 	function transition(newMode, replace = false) {
