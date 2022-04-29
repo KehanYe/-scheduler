778fae9e570ed2ccb308bbd708173278b5d030cd
KehanYe <kehanfu@gmail.com>
2022-04-28 18:56:22 -0600

    setup cypress with intial tests

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,12 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    // eslint-disable-next-line no-undef
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    // eslint-disable-next-line no-undef
+    cy.visit("/");
+  });
+});
+
